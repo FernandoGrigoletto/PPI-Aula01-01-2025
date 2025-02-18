3a95779ec07f60b11417fcc52f47c0063685122a
Fernando Grigoletto <fernandogrigo412@gmail.com>
2025-02-17 22:36:24 -0300

    Atualização

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 import express from "express";
-import autenticar from  "./seguranca/autenticar.js";   
+import autenticar from "./seguranca/autenticar.js";   
+import session from 'express-session';
 
 const porta = 3000;
 const localhost = "0.0.0.0";
 
 const app = express();
+
+app.use(session({
+    secret: "Gr1g0l3tt0",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 15
+    }
+}));
+
+app.get("/login", (requisicao, resposta) => {
+    resposta.redirect('/login.html');
+})
+
+app.get("/login", (requisicao, resposta) => {
+    resposta.redirect('/login.html');
+})
 
 app.use(express.static("./publico"));
 
